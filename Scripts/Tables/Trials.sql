ca994c85b754d24e1480c4bd2e9d4ec7029f6f2c
Sathya-Bala <sathyabala1934@gmail.com>
2025-09-21 14:29:07 +0530

    Inserted values

--- Scripts/Tables/Trials.sql
+++ Scripts/Tables/Trials.sql
@@ -5,3 +5,11 @@ CREATE TABLE Trials (
   start_date DATE,
   end_date DATE
 );
+
+INSERT INTO Trials (trial_name, phase, start_date, end_date)
+VALUES
+('TrialNet Diabetes Study', 'II', '2025-01-01', '2026-12-31'),
+('TrialNet Immunotherapy Study', 'III', '2025-03-01', '2027-06-30'),
+('TrialNet Early Screening Study', 'I', '2024-09-01', '2025-09-01');
+
+SELECT * FROM Trials;
