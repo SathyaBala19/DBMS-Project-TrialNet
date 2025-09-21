c47ea1836fc524888e8f4c982b9e36c1440db1e2
Sathya-Bala <sathyabala1934@gmail.com>
2025-09-21 19:55:37 +0530

    index and optimization

---
+++ Scripts/TEST/Indexing & Optimization test.sql
@@ -0,0 +1,12 @@
+﻿-- Participants → Visits → Labs query
+SELECT 
+    p.participant_code,
+    v.visit_date,
+    l.test_name,
+    l.result_value
+FROM Participants p
+JOIN Visits v ON p.participant_id = v.participant_id
+JOIN Labs l ON v.visit_id = l.visit_id
+WHERE l.test_code = 'GLU'
+  AND v.visit_date BETWEEN '2025-01-01' AND '2025-12-31'
+ORDER BY v.visit_date;
