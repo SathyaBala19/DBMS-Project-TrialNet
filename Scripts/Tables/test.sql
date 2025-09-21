f4cc7576d1d029426e858d73ce511164312eb612
Sathya-Bala <sathyabala1934@gmail.com>
2025-09-21 12:50:35 +0530

    added test table

---
+++ Scripts/Tables/test.sql
@@ -0,0 +1,16 @@
+USE [TrialNet]
+GO
+
+/****** Object:  Table [dbo].[test]    Script Date: 21-09-2025 12:49:30 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[test](
+	[id] [int] NULL
+) ON [PRIMARY]
+GO
+
+
